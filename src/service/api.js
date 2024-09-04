4be2bec2556410dc04d2b274e0a0f2f1abc8ea9e
bharatayasa <bharatayasa40@gmail.com>
2024-09-04 16:45:27 +0800

    refresh netlify

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const Api = axios.create({
-    // baseURL: 'http://0.0.0.0:3000'
-    baseURL: 'https://chilitify-433503.lm.r.appspot.com/'
+    baseURL: 'http://0.0.0.0:3000'
+    // baseURL: 'https://chilitify-433503.lm.r.appspot.com/'
 })
 
 export default Api
